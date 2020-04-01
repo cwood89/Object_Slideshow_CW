3988e4575e76cf01cf090e946907709bf534366c
woodtop <cwood89@mac.com>
2020-04-01 15:48:23 -0400

    reworked nextPhoto functionality and added play/pause methods

--- object-lab.js
+++ object-lab.js
@@ -2,44 +2,60 @@ let slideShow = {
   photoList: ['photo1', 'photo2', 'photo3', 'photo4', 'photo5',],
   currentPhotoIndex: 0,
   nextPhoto: function () {
-    this.currentPhotoIndex++;
-    if (this.photoList[this.currentPhotoIndex]) {
+    if (this.currentPhotoIndex < this.photoList.length - 1) {
+      this.currentPhotoIndex++;
       console.log(this.photoList[this.currentPhotoIndex]);
+      return this.photoList[this.currentPhotoIndex];
     } else {
-      console.log("End of slideshow.")
-      this.currentPhotoIndex--;
+      console.log("End of slideshow.");
+      this.pause();
+      return "End of slideshow.";
     }
   },
   prevPhoto: function () {
-    this.currentPhotoIndex--;
-    if (this.photoList[this.currentPhotoIndex]) {
+    if (this.currentPhotoIndex > 0) {
+      this.currentPhotoIndex--;
       console.log(this.photoList[this.currentPhotoIndex]);
+      return this.photoList[this.currentPhotoIndex];
     } else {
       console.log("Beginning of slideshow.")
-      this.currentPhotoIndex++;
+      return "Beginning of slideshow.";
     }
   },
   getCurrentPhoto: function () {
-    console.log(this.photoList[this.currentPhotoIndex]);
+    return this.photoList[this.currentPhotoIndex];
+  },
+  playInterval: null,
+  play: function () {
+    var self = this;
+    this.playInterval = setInterval(function () {
+      self.nextPhoto();
+    }, 2000);
+  },
+  pause: function () {
+    var self = this;
+    clearInterval(self.playInterval);
   }
-
 }
 
-// ==== testing methods ====
+slideShow.play();
 
-slideShow.getCurrentPhoto(); // photo1
-slideShow.nextPhoto(); // photo2
-slideShow.nextPhoto(); // photo3
-slideShow.prevPhoto(); // photo2
-slideShow.prevPhoto(); // photo1
-slideShow.prevPhoto(); // Beginning of slideshow
-slideShow.getCurrentPhoto(); // photo1
-slideShow.nextPhoto(); // photo2
-slideShow.nextPhoto(); // photo3
-slideShow.nextPhoto(); // photo4
-slideShow.nextPhoto(); // photo5
-slideShow.nextPhoto(); // end of slideshow
-slideShow.nextPhoto(); // end of slideshow
+// ==== testing methods ====
+// console.log(
+//   slideShow.getCurrentPhoto(), // photo1
+//   slideShow.nextPhoto(), // photo2
+//   slideShow.nextPhoto(), // photo3
+//   slideShow.prevPhoto(), // photo2
+//   slideShow.prevPhoto(), // photo1
+//   slideShow.prevPhoto(), // Beginning of slideshow
+//   slideShow.getCurrentPhoto(), // photo1
+//   slideShow.nextPhoto(), // photo2
+//   slideShow.nextPhoto(), // photo3
+//   slideShow.nextPhoto(), // photo4
+//   slideShow.nextPhoto(), // photo5
+//   slideShow.nextPhoto(), // end of slideshow
+//   slideShow.nextPhoto() // end of slideshow
+// );
 
 
 
